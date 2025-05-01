4d476930524924a5897c16096d52fa92c7b87b6c
Sai Zaw Myint <96133665+SaiZawMyint@users.noreply.github.com>
2025-05-01 15:51:58 +0630

    initial commit

---
+++ src/main/java/io/github/flexitech_realtime_provider/realtime_service/RealtimeServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.flexitech_realtime_provider.realtime_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/flexitech_realtime_provider/realtime_service/RealtimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.flexitech_realtime_provider.realtime_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
